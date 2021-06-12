c825e047394eff96c846c4b4a22d0420d4283190
unknown <shubhankart@bigrattle.com>
2021-06-12 21:49:15 +0530

    Added cors

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const connectDb = require('./db/index')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
+app.use(cors())
 connectDb()
 
 app.use('/api/user', require('./routes/users'))
